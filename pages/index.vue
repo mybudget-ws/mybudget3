a97226ba73f6d67d7ebd95db847060a27441f3cf
blackchestnut <alexander.kalinichev@gmail.com>
2025-06-15 21:45:42 +0300

    Update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,10 @@
 <template>
   <div>
-    <h1>Мой Бюджет</h1>
-    <h2>Домашняя бухгалтерия онлайн</h2>
-    <button
-      type="button"
-      class="btn"
-      data-bs-toggle="tooltip"
-      data-bs-placement="top"
-      title="Tooltip on top"
-    >
-      Tooltip on top
-    </button>
+    <h1 class='display-3 fw-medium'>Домашняя бухгалтерия</h1>
+    <h2 class='display-6 fw-normal text-secondary'>Инструмент для учета повседневных доходов и расходов</h2>
+    <div>
+      <span class='badge bg-orange text-orange-fg me-1'>Версия III</span>
+      <span class='badge bg-green text-green-fg'>В разработке</span>
+    </div>
   </div>
 </template>
