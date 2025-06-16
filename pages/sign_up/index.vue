295c6b3b8bc3daf54ffe4fb4ae4cec93af65a691
blackchestnut <alexander.kalinichev@gmail.com>
2025-06-16 22:10:56 +0300

    Add sugn_up template

--- pages/sign_up/index.vue
+++ pages/sign_up/index.vue
@@ -6,9 +6,7 @@
     <div class='container container-tight py-4'>
       <div class='card card-md'>
         <div class='card-body'>
-          <h2 class='h2 text-center mb-4'>
-            Вход в Мой Бюджет
-          </h2>
+          <h2 class='h2 text-center mb-4'>Регистрация</h2>
           <form action="./" method="get" autocomplete="off" novalidate="">
             <div class='mb-3'>
               <label class='form-label'>Email</label>
@@ -20,12 +18,7 @@
               >
             </div>
             <div class='mb-2'>
-              <label class='form-label'>
-                Пароль
-                <span class='form-label-description'>
-                  <a href='/forgot-password'>Не помню пароль</a>
-                </span>
-              </label>
+              <label class='form-label'>Пароль</label>
               <div class='input-group input-group-flat'>
                 <input
                   type='password'
@@ -40,15 +33,15 @@
                 type='submit'
                 class='btn btn-primary w-100'
               >
-                Войти
+                Создать Мой Бюджет
               </button>
             </div>
           </form>
         </div>
-        <div class="hr-text">Впервые на сайте?</div>
-        <div class="card-body">
-          <NuxtLink to='/sign_up' class='btn btn-4 w-100'>
-            Зарегистрироваться
+        <div class='hr-text'>Уже есть аккаунт?</div>
+        <div class='card-body'>
+          <NuxtLink to='/sign_in' class='btn btn-4 w-100'>
+            Войти
           </NuxtLink>
         </div>
       </div>
