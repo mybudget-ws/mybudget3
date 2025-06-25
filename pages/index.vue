ff26dd2a2bf0c3aff967b2e69aac7c101484a753
blackchestnut <alexander.kalinichev@gmail.com>
2025-06-25 22:30:48 +0300

    Add sign_in/up on index

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,23 @@
   <div>
     <h1 class='display-4 fw-medium'>Домашняя бухгалтерия</h1>
     <h2 class='display-6 fw-normal text-secondary'>Инструмент для учета повседневных доходов и расходов</h2>
-    <div>
-      <span class='badge bg-orange text-orange-fg me-1'>Версия III</span>
-      <span class='badge bg-green text-green-fg'>В разработке</span>
+    <div class='row mb-4'>
+      <div class='col'>
+        <span class='badge bg-orange text-orange-fg me-1'>Версия III</span>
+        <span class='badge bg-green text-green-fg'>В разработке</span>
+      </div>
+    </div>
+    <div class='row'>
+      <div class='col-auto'>
+        <NuxtLink class='btn btn-primary' to='/sign_up'>
+          Регистрация
+        </NuxtLink>
+      </div>
+      <div class='col-auto'>
+        <NuxtLink class='btn btn-4' to='/sign_in'>
+          Вход
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
