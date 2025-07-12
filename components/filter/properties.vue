eb20d77bf715b1814c56541de05001e033f82d02
blackchestnut <alexander.kalinichev@gmail.com>
2025-07-12 21:37:38 +0300

    Add filter by projects

--- components/filter/properties.vue
+++ components/filter/properties.vue
@@ -1,8 +1,81 @@
+<script setup>
+import api from '~/lib/api';
+
+const route = useRoute();
+const router = useRouter();
+const { token } = useAuth();
+
+const isLoading = ref(true);
+const items = ref([]);
+const selectedIds = ref(new Set());
+
+const load = async () => {
+  isLoading.value = true
+  try {
+    const result = await api.properties(token.value);
+    if (result) {
+      items.value = result;
+      const queryIds = route.query.properties?.toString().split(',') || [];
+      selectedIds.value = new Set(queryIds.map(id => Number(id)));
+    } else {
+      console.log('TODO: error');
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    isLoading.value = false
+  }
+};
+
+const toggleSelection = (id) => {
+  if (selectedIds.value.has(id)) {
+    selectedIds.value.delete(id);
+  } else {
+    selectedIds.value.add(id);
+  }
+
+  router.replace({
+    query: {
+      ...route.query,
+      properties: Array.from(selectedIds.value).join(','),
+    },
+  });
+};
+
+const visibleItems = computed(() => (
+  items.value.filter(v => v.isHidden === false)
+));
+
+watch(
+  () => token.value,
+  (val) => {
+    if (val) load();
+  },
+  { immediate: true }
+);
+</script>
+
 <template>
   <div class='card mb-3'>
-    <div class='card-body p-3'>
-      <div class='subheader'>Имущество</div>
-      <div>TODO</div>
+    <PlaceholderLoadingFilters v-if='isLoading' />
+
+    <div v-else class='card-body p-3 pb-0'>
+      <div class='subheader mb-3'>Имущество</div>
+      <div v-for='item in visibleItems' :key='item.id'>
+        <label class='form-check'>
+          <input
+            class='form-check-input'
+            type='checkbox'
+            :checked='selectedIds.has(item.id)'
+            @change='toggleSelection(item.id)'
+          >
+          <span class='form-check-label'>
+            {{ item.name }}
+          </span>
+        </label>
+      </div>
     </div>
   </div>
 </template>
+
+
