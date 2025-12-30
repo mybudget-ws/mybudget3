7a918aa9ed32df6422b70ff13ea5be4d0a1a92ad
blackchestnut <alexander.kalinichev@gmail.com>
2025-12-30 22:08:54 +0300

    Add show_callback to modal/base

--- components/modal/base.vue
+++ components/modal/base.vue
@@ -1,6 +1,21 @@
 <script setup>
 const props = defineProps({
   id: String,
+  showCallback: Function,
+});
+
+onMounted(() => {
+  const modalElement = document.getElementById(props.id);
+  if (modalElement && props.showCallback) {
+    modalElement.addEventListener('shown.bs.modal', props.showCallback);
+  }
+});
+
+onUnmounted(() => {
+  const modalElement = document.getElementById(props.id);
+  if (modalElement && props.showCallback) {
+    modalElement.removeEventListener('shown.bs.modal', props.showCallback);
+  }
 });
 </script>
 
