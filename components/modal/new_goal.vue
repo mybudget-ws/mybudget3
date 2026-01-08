ece9f757d2e0cc6c1f4501381b006f54e9f238e3
blackchestnut <alexander.kalinichev@gmail.com>
2026-01-08 20:47:20 +0300

    Add template of goal new form

--- components/modal/new_goal.vue
+++ components/modal/new_goal.vue
@@ -1,12 +1,11 @@
 <script setup>
 import api from '~/lib/api';
 
-const DEFAULT_AMOUNT = 100000;
 const DEFAULT_POSITION = 1;
 
 const { token } = useAuth();
 const goalName = ref('');
-const goalAmount = ref(DEFAULT_AMOUNT);
+const goalAmount = ref('');
 const goalDate = ref(new Date());
 const goalAccountIds = ref([]);
 const goalPosition = ref(DEFAULT_POSITION);
@@ -60,7 +59,7 @@ watch(
   () => props.item,
   (val) => {
     goalName.value = val?.name ?? '';
-    goalAmount.value = val?.amount ?? DEFAULT_AMOUNT;
+    goalAmount.value = val?.amount ?? '';
     goalDate.value = val?.dueDateOn ?? new Date();
     goalAccountIds.value = val?.accountIds ?? [];
     goalPosition.value = val?.position ?? DEFAULT_POSITION;
@@ -80,16 +79,50 @@ watch(
       </div>
 
       <div class='modal-body'>
-        <div class='mb-3'>
-          <Label required>Название</Label>
-          <Input
-            required
-            type='text'
-            class='form-control'
-            placeholder='Новая цель'
-            :disabled='isSubmitting'
-            v-model='goalName'
-          />
+        <div class='row'>
+          <div class='col-md-12 col-lg-6 mb-3'>
+            <Label required>Название</Label>
+            <Input
+              required
+              type='text'
+              class='form-control'
+              placeholder='Новая цель'
+              :disabled='isSubmitting'
+              v-model='goalName'
+            />
+          </div>
+          <div class='col-md-12 col-lg-6 mb-3'>
+            <Label required>Дата</Label>
+            <InputDate
+              :disabled='isSubmitting'
+              v-model='goalDate'
+            />
+          </div>
+        </div>
+
+        <div class='row'>
+          <div class='col-md-12 col-lg-6 mb-3'>
+            <Label required>Величина</Label>
+            <Input
+              required
+              type='number'
+              class='form-control'
+              placeholder='Нужно накопить'
+              :disabled='isSubmitting'
+              v-model='goalAmount'
+            />
+          </div>
+          <div class='col-md-12 col-lg-6 mb-3'>
+            <Label required>Позиция в списке</Label>
+            <Input
+              required
+              type='number'
+              class='form-control'
+              placeholder='1'
+              :disabled='isSubmitting'
+              v-model='goalPosition'
+            />
+          </div>
         </div>
       </div>
 
