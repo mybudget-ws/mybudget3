1a6e854b979e0e3957e39463070694369eaeaeee
Dodgen <toucan305@gmail.com>
2026-05-07 00:49:23 +0300

    clean up

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -49,38 +49,38 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="card">
-    <div class="row g-0">
+  <div class='card'>
+    <div class='row g-0'>
       <ProfileMenu />
-      <div class="col-sm-12 col-lg-9 col-xl-10">
-        <div class="card-body">
+      <div class='col-sm-12 col-lg-9 col-xl-10'>
+        <div class='card-body'>
           <h1>Ваш профиль</h1>
-          <div v-if="isLoading" class="spinner-border" />
+          <div v-if='isLoading' class='spinner-border' />
           <div v-else>
-            <div class="row mb-3">
-              <div class="col-md-6 col-lg-4">
+            <div class='row mb-3'>
+              <div class='col-md-6 col-lg-4'>
                 <Input
-                  type="email"
-                  v-model="email"
+                  type='email'
+                  v-model='email'
                   disabled
                 />
               </div>
             </div>
-            <div class="row mb-3">
-              <div class="col-md-6 col-lg-4">
-                <select v-model="selectedCurrency" class="form-select">
+            <div class='row mb-3'>
+              <div class='col-md-6 col-lg-4'>
+                <select v-model='selectedCurrency' class='form-select'>
                   <option disabled value="">Выберите валюту</option>
                   <option
-                    v-for="c in currencies"
-                    :key="c.value"
-                    :value="c.value"
+                    v-for='c in currencies'
+                    :key='c.value'
+                    :value='c.value'
                   >
                     {{ c.label }}
                   </option>
                 </select>
               </div>
             </div>
-            <button class="btn btn-primary">
+            <button class='btn btn-primary'>
               Сохранить
             </button>
           </div>
@@ -89,11 +89,11 @@ onMounted(async () => {
     </div>
   </div>
 
-  <div class="d-flex justify-content-end">
+  <div class='d-flex justify-content-end'>
     <Button
-      v-if="isSignedIn"
-      class="btn-outline-danger mt-3"
-      @click="onSignOut"
+      v-if='isSignedIn'
+      class='btn-outline-danger mt-3'
+      @click='onSignOut'
     >
       Выход
     </Button>
