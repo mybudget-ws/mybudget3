efeab63ba52fbdfc4b423e7fe4e6b175a279abe2
Dodgen <toucan305@gmail.com>
2026-05-25 15:31:42 +0300

    new-style-for-button-load-more

--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -559,13 +559,17 @@ watch(
                 </tbody>
               </table>
             </div>
-            <div class="card-footer d-flex justify-content-center align-items-center">
+            <div
+              class="card-footer bg-transparent pt-0 pb-2"
+              :class="{ 'border-0': !hasMore && !isLoadingMore }"
+            >
               <i v-if="isEmpty" class="text-secondary">
                 Похоже таких операций ещё нет
               </i>
+
               <button
                 v-else
-                class="btn btn-ghost-secondary"
+                class="btn btn-action btn-sm text-secondary w-100 mt-2 border-0"
                 :disabled="!hasMore || isLoadingMore"
                 @click="loadMore"
               >
